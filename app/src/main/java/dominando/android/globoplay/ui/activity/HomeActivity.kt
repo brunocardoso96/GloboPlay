bc461e05bdf4583dd1aa0925a2b90c416e0f2d6d
Bruno W Nascimento <brunowcnascimento@gmail.com>
2021-07-05 12:06:07 -0300

    [02] - Implementando o Toolbar

--- app/src/main/java/dominando/android/globoplay/ui/activity/HomeActivity.kt
+++ app/src/main/java/dominando/android/globoplay/ui/activity/HomeActivity.kt
@@ -2,15 +2,12 @@ package dominando.android.globoplay.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dominando.android.globoplay.databinding.ActivityMainBinding
-
-private lateinit var binding: ActivityMainBinding
+import dominando.android.globoplay.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 }
