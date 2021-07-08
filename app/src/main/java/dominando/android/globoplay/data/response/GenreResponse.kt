06503f212a90a4ff2eb155fda2a805034563efdb
Bruno W Nascimento <brunowcnascimento@gmail.com>
2021-07-08 11:26:40 -0300

    [11] - Implementando o GenreResponse

---
+++ app/src/main/java/dominando/android/globoplay/data/response/GenreResponse.kt
@@ -0,0 +1,18 @@
+package dominando.android.globoplay.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class GenreResponse(
+
+	@SerializedName("genres")
+	val genres: List<GenresItem>
+)
+
+data class GenresItem(
+
+	@SerializedName("name")
+	val name: String,
+
+	@SerializedName("id")
+	val id: Int
+)
