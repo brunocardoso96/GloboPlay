2c756fec3e947cfc98b4c938b3bd9890285366ec
Bruno W Nascimento <brunowcnascimento@gmail.com>
2021-07-10 15:40:58 -0300

    [23] - Pequenos ajustas no layout

--- app/src/main/java/dominando/android/globoplay/ui/feature/moviedetail/adapter/MovieInfoAdapter.kt
+++ app/src/main/java/dominando/android/globoplay/ui/feature/moviedetail/adapter/MovieInfoAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import dominando.android.globoplay.R
 import dominando.android.globoplay.ui.feature.moviedetail.fragment.MovieInfoFragment
 
 private val TAB_TITLES = arrayOf(
